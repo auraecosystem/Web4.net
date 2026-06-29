5f884ea89962a28ffbf2c8f832d03243594a860d
Seriki Walter Yakub <sandraisom@icloud.com>
2026-06-29 13:33:47 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,6 @@
 curl https://dropmail.me/api/graphql/af_AQFqQmJusEKqeszSQa6PbTdKW8DB8HoSZkndCY_o -H 'Content-Type: application/json' \
  -d '{"query":"mutation {introduceSession {id, expiresAt, addresses {fil1davykag3@mail2me.co}}}"}'
+
+# DropMail API Example
+curl -X GET "https://dropmail.me/api" \
+  -H "Content-Type: application/json"
