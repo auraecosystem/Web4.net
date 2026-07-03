f146b2b8f18537e01e23603df99784d2d3fcd06d
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-03 18:19:43 +0100

    Create engine.ts

---
+++ engine.ts
@@ -0,0 +1,37 @@
+const engine = new QueryEngine();
+
+engine.execute({
+    language: "sql",
+    query: "...",
+    data
+});
+
+engine.execute({
+    language: "jsoniq",
+    query: "...",
+    data
+});
+
+engine.execute({
+    language: "jsonpath",
+    query: "...",
+    data
+});
+
+engine.execute({
+    language: "jmespath",
+    query: "...",
+    data
+});
+
+engine.execute({
+    language: "graphql",
+    query: "...",
+    data
+});
+
+engine.execute({
+    language: "jq",
+    query: "...",
+    data
+});
